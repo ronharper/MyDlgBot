3c5b8e328eb7f51a20725e3f4946df733954b42d
ronharper <ronharper@outlook.com>
2017-05-11 16:52:44 -0700

    foo

--- app.js
+++ app.js
@@ -22,10 +22,10 @@ server.listen(process.env.port || 3978, function () {
 // Setup bot and root waterfall
 
 var connector = new builder.ChatConnector(
-//     {
-//     appId: MICROSOFT_APP_ID,
-//     appPassword: MICROSOFT_APP_PASSWORD
-// }
+    {
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+}
 );
 server.post("/api/messages", connector.listen());
 var bot = new builder.UniversalBot(connector, [
